2d52218bb24c3be706cbc58bd0798df5f94f6341
budou114 <58349030+budou114@users.noreply.github.com>
2022-06-12 16:56:42 +0900

    おまけ(useMemo)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useMemo } from "react";
 import { ChildArea } from "./ChildArea";
 
 export default function App() {
@@ -16,6 +16,9 @@ export default function App() {
   const onClickOpen = () => setOpen(!open);
 
   const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+
+  const temp = useMemo(() => 1 + 3, []);
+  console.log(temp);
 
   return (
     <div className="App">
