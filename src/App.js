256f524b8f179b887d2a4404419c02831ff10564
budou114 <58349030+budou114@users.noreply.github.com>
2022-06-12 16:14:13 +0900

    props機能の追加

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import { useState } from "react";
+import { ChildArea } from "./ChildArea";
 
 export default function App() {
   console.log('App');
   const [count, setCount] = useState(0);
+  const [text, setText] = useState('');
+  const [open, setOpen] = useState(false);
 
   const onClickCountUp = () => {
     setCount(count + 1);
   }
+
+  const onChangeText = (e) => setText(e.target.value);
+
+  const onClickOpen = () => setOpen(!open);
 
   return (
     <div className="App">
       <h1>Hello react</h1>
       <p>{count}</p>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} />
     </div>
   );
 }
